fb5867a868ccea29cca051f74076206680864709
dinger22 <dingyinghao@hotmail.com>
2020-01-22 22:00:36 -0500

    Use spring to manage beans

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,6 +2,7 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 
 @SpringBootApplication
@@ -9,11 +10,13 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		
-		BinarySearchImpl binarySearch = new BinarySearchImpl(new BubbleSortAlgorithm());
+		ApplicationContext applicationContext = SpringApplication.run(DemoApplication.class, args);
+		BinarySearchImpl binarySearch = applicationContext.getBean(BinarySearchImpl.class);
+
 		int result = binarySearch.binarySearch(new int[3], 3);
 		System.out.println(result);
 		
-		SpringApplication.run(DemoApplication.class, args);
+		
 	}
 	
 }
