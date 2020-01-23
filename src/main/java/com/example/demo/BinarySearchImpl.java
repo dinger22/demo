b3a6bfe807a57f11297834b140ddb8f9995edb3a
dinger22 <dingyinghao@hotmail.com>
2020-01-23 00:08:40 -0500

    qualifier

--- src/main/java/com/example/demo/BinarySearchImpl.java
+++ src/main/java/com/example/demo/BinarySearchImpl.java
@@ -1,11 +1,13 @@
 package com.example.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class BinarySearchImpl {
 	@Autowired
+	@Qualifier("quick")
 	private SortAlgorithm quickSortAlgorithm;
 
 	public int binarySearch(int[] numbers, int numberToSearchFor) {
--- src/main/java/com/example/demo/BubbleSortAlgorithm.java
+++ src/main/java/com/example/demo/BubbleSortAlgorithm.java
@@ -1,8 +1,10 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
+@Qualifier("bbubble")
 public class BubbleSortAlgorithm implements SortAlgorithm{
 	public int[] sort(int[] numbers) {
 		return numbers;
